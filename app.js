ed34541528927df05ff5e3b06ba3669f811c9b53
Andrew Odewahn <odewahn@oreilly.com>
2014-06-27 14:49:42 -0400

    adding angular

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 (function(){
 	
-   var dat = [];
-
-   function setDat(d) {
-      dat = d;	
-   }
-
    var app = angular.module('splashPage', []);
-   app.controller('SplashController', function() {
-      $.ajax({
-	     url: "index.json",
-	     dataType: 'json',
-	     async: false,
-	     success: function(data){
-		    setDat(data);
-		    console.log(data);
-	     }
-      });
-      this.hi = "Howdy, andrew!"
-      this.splashdata = dat;
-
-   })	
+   app.controller('SplashController', ['$http', function($http) {
+	  var contents = this;
+	  contents.splashdata = {};
+	  $http.get("/index.json").success(function(data) {
+		contents.splashdata = data;
+	  });
+   }]);	
 })();
