1d880eeb59eda480b82679ca5f7c28ceb5fd8647
Andrew Odewahn <odewahn@oreilly.com>
2014-06-29 06:44:41 -0400

    moved mailchimp form into a directive

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
 (function(){
-	
+		
    var app = angular.module('splashPage', []);
+
+   
+   app.directive('mailchimp', function() {
+      return {
+	     restrict: 'E',
+		 templateUrl: 'app/mailchimp.html'
+	  };
+   });
+
+   // Main controller to load the data for the homepage
    app.controller('SplashController', ['$http', function($http) {
 	  var contents = this;
 	  contents.splashdata = {};
 	  $http.get("app/index.js").success(function(data) {
 		contents.splashdata = data;
 	  });
-   }]);	
+   }]);
+	
 })();
